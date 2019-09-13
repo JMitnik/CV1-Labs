3a7e14c437ae7d89aec79874acf1631c4d20a853
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-13 14:19:07 +0200

    Edited

--- lab1/photometric/check_integrability.m
+++ lab1/photometric/check_integrability.m
@@ -6,33 +6,20 @@ function [ p, q, SE ] = check_integrability( normals )
 %   SE : Squared Errors of the 2 second derivatives
 
 % initalization
-p = zeros(size(normals));
-q = zeros(size(normals));
-SE = zeros(size(normals));
 [h,w,d] = size(normals);
+p = zeros(h,w);
+q = zeros(h,w);
+SE = zeros(h,w);
 % ========================================================================
 % YOUR CODE GOES HERE
-% Loop over each normal to calculate p's and q's
-for r = 1:h
-    for c = 1:w
-    % Compute p and q, where
-    % p measures value of df / dx = a(x,y)/c(x,y)
-    p_ = normals(r,c,1) / normals(r,c,3);
-    % q measures value of df / dy = 
-    q_ = normals(r,c,2) / normals(r,c,3);
-
-    end
-end
+% Calculate p's and q's, the derivatives
+p = normals(:,:,1) ./ normals(:,:,3);
+q = normals(:,:,2) ./ normals(:,:,3);
 
 % ========================================================================
-
-
-
+% Convert all the NaN values to zeroes in the matrices
 p(isnan(p)) = 0;
 q(isnan(q)) = 0;
-
-
-
 % ========================================================================
 % YOUR CODE GOES HERE
 % approximate second derivate by neighbor difference
