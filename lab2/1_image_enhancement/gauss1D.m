ad54cfd8801d187e72e63253ebf674ba160c6b9e
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-19 19:01:32 +0200

    Update gauss1D.m

--- lab2/1_image_enhancement/gauss1D.m
+++ lab2/1_image_enhancement/gauss1D.m
@@ -4,4 +4,8 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    x = linspace(-floor(kernel_size/2),floor(kernel_size/2), kernel_size);
+    
+    gauss = @(i) 1/(sigma*sqrt(2*pi))* exp(-(i.^2)/(2*(sigma^2)));
+    G = gauss(x) ./ sum(gauss(x));
 end
