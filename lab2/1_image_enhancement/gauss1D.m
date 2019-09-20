426ef370fd2daeae48f1557993c60dc6780eae70
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-20 10:48:14 +0200

    Implemented gauss2D filter

--- lab2/1_image_enhancement/gauss1D.m
+++ lab2/1_image_enhancement/gauss1D.m
@@ -4,8 +4,9 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
-    x = linspace(-floor(kernel_size/2),floor(kernel_size/2), kernel_size);
     
+    x = linspace(-floor(kernel_size/2),floor(kernel_size/2), kernel_size);
+    % Function for the gaussian distribution
     gauss = @(i) 1/(sigma*sqrt(2*pi))* exp(-(i.^2)/(2*(sigma^2)));
     G = gauss(x) ./ sum(gauss(x));
 end
--- lab2/1_image_enhancement/gauss2D.m
+++ lab2/1_image_enhancement/gauss2D.m
@@ -1,3 +1,12 @@
 function G = gauss2D( sigma , kernel_size )
     %% solution
+    G = transpose(gauss1D(sigma, kernel_size)) * gauss1D(sigma, kernel_size);
+    % REMOVE THIS LATER, THIS IS JUST TO CHECK!
+    I = imread('images/image1_gaussian.jpg');
+    %apply own filter
+    Iblur = imfilter(I,G);
+    %predefined matlab filter
+    Iblur2 = imgaussfilt(I,2);
+    montage({I,Iblur, Iblur2});
+    
 end
