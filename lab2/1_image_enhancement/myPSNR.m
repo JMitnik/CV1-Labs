e4fbdd67edb8321a1e83f825ef14decf5d34a24a
csirika <55434208+csirika@users.noreply.github.com>
2019-09-21 14:30:51 +0200

    Update myPSNR.m

--- lab2/1_image_enhancement/myPSNR.m
+++ lab2/1_image_enhancement/myPSNR.m
@@ -1,5 +1,30 @@
-function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
+function [ PSNR] = myPSNR( orig_image, approx_image )
 
+%Getting the sizes of the image
+[r,c]=size(orig_image);
+orig_image_dbl=im2double(orig_image);
+approx_image_dbl=im2double(approx_image);
+
+%Computing mean squared error
+MSE=0;
+    for i=1:r
+        for j=1:c
+            MSE=MSE+(orig_image_dbl(i,j)-approx_image_dbl(i,j))^2;
+        end
+    end
+    MSE=MSE/(r*c);
+    
+%Finding I_max
+I_max=0;
+    for i=1:r
+        for j=1:c
+            if (orig_image_dbl(i,j)>I_max)
+                I_max=orig_image_dbl(i,j);
+            end
+        end
+    end
+        
+
+%Computing the PSNR
+PSNR=10*log10(I_max*I_max/MSE);
 end
-
