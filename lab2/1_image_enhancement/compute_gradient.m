d697010e4bf36c7e00472188df3d43350d462221
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-23 12:00:48 +0200

    Implemented Sobel Filter
    Sobel filter get the change in X and Y directions. Also detects the magnitube and direction of the gradient per pixel.

--- lab2/1_image_enhancement/compute_gradient.m
+++ lab2/1_image_enhancement/compute_gradient.m
@@ -1,5 +1,21 @@
 function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
-fprintf('Not implemented\n')
+
+sobelX = [1 0 -1; 2 0 -2; 1 0 -1];
+sobelY = sobelX';
+
+Gx = imfilter(image, sobelX, 'conv');
+Gy = imfilter(image, sobelY, 'conv');
+im_magnitude = hypot(double(Gx), double(Gy));
+im_direction = atan(double(Gy./Gx));
+
+figure(1);
+imshowpair(Gx, Gy, 'montage');
+title('Directional Gradients Gx and Gy, Using Sobel Method');
+
+figure(2);
+imshowpair(im_magnitude, im_direction, 'montage');
+title('Gradient Magnitude and Direction per Pixel');
+
 
 end
 
