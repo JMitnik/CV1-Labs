cd527f414a286ba3516a000e14bee215d5c1ab20
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-23 15:08:38 +0200

    compute_gradient: imfilter -> conv2

--- lab2/1_image_enhancement/compute_LoG.m
+++ lab2/1_image_enhancement/compute_LoG.m
@@ -3,7 +3,12 @@ function imOut = compute_LoG(image, LOG_type)
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
+        h = fspecial('gaussian',5, 0.5);
+        image_smooth = imfilter(image, h, 'conv');
+        
+        image_laplace = del2(image_smooth);%imfilter(image_smooth, h, 'conv');
+        figure(1);
+        imshowpair(image_smooth, image_laplace, 'montage');
 
     case 2
         %method 2
--- lab2/1_image_enhancement/compute_gradient.m
+++ lab2/1_image_enhancement/compute_gradient.m
@@ -5,15 +5,15 @@ sobelX = [1 0 -1; 2 0 -2; 1 0 -1];
 sobelY = sobelX';
 
 % Apply the kernels to the image and get Gx, Gy, magnitude and direction
-Gx = imfilter(image, sobelX, 'conv');
-Gx = rescale(Gx, 0, 255);
-Gy = imfilter(image, sobelY, 'conv');
-Gy = rescale(Gy, 0, 255);
+Gx = conv2(image, sobelX);
+Gy = conv2(image, sobelY);
 
-im_magnitude = round(hypot(double(Gx), double(Gy)));
+
+im_magnitude = round(rescale(hypot(double(Gx), double(Gy)), 0, 255));
 im_magnitude (isnan(im_magnitude)) = 0;
 im_direction = round(rescale(atan(double(Gy./Gx)), 0, 255));
 im_direction (isnan(im_direction)) = 0;
+
 %D Display the results
 figure(1);
 imshowpair(Gx, Gy, 'montage');
