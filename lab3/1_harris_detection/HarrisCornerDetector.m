c1cb366b0cbddaabfe572dce15cd1db336db05b1
de Marez Oyens, P.M.W. (Pieter) <oyenspieter@gmail.com>
2019-09-26 19:08:43 +0200

    H doesn't work yet but there's at least something

--- lab3/1_harris_detection/HarrisCornerDetector.m
+++ lab3/1_harris_detection/HarrisCornerDetector.m
@@ -11,26 +11,32 @@ sobelX = [1 0 -1; 2 0 -2; 1 0 -1];
 sobelY = sobelX';
 sigma = 0.5
 % Apply first-order Gaussian, aka Sobel filter to find edge in x-dir
-I_x = imfilter(I, sobelX, 'conv');
-I_x = I_x .^ 2;
-I_x = imgaussfilt(I_x, sigma);
-
-I_y = imfilter(I, sobelY, 'conv');
-I_y = I_y .^ 2;
-I_y = imgaussfilt(I_y, sigma);
+I_x = imfilter(I, sobelY, 'conv');
+I_x_sq = I_x .^ 2;
+A = imgaussfilt(I_x_sq, sigma);
+% Apply first-order Gaussian, aka Sobel filter to find edge in y-dir
+I_y = imfilter(I, sobelX, 'conv');
+I_y_sq = I_y .^ 2;
+C = imgaussfilt(I_y_sq, sigma);
 
 I_xy = I_x .* I_y;
-I_xy = imgaussfilt(I_xy, sigma);
+B = imgaussfilt(I_xy, sigma);
+
+H = (A .* C - B.^2) - 0.04*(A+C).^2;
 
 figure(1);
-imshow(I_x);
+imshow(A);
 title('I_x');
 figure(2);
-imshow(I_y);
+imshow(C);
 title('I_y');
 
 figure(3);
-imshow(I_xy);
+imshow(B);
 title('I_{xy}');
+
+figure(4);
+imshow(H);
+title('H');
 
 end
