f65ede72222c063629798dbf4175723cf02c8a8b
Jonathan Mitnik <jmitnik@gmail.com>
2019-09-30 00:13:11 +0200

    Pseudo-code

--- lab3/3_feature_tracking/tracking.m
+++ lab3/3_feature_tracking/tracking.m
@@ -1,5 +1,20 @@
 % INSERT CODE HERE
 
-% Step 1: Use the harris algorithm for the first layer of a series of frames
+PATH_TO_IMG_FOLDER = '../pingpong';
+SIGMA= 1;
+NEIGHBOURS = 10;
+THRESHOLD = 0.05;
 
-% Step 2: Track the p
+I1 = imread(fullfile(PATH_TO_IMG_FOLDER, '0001.jpeg'));
+I2 = imread(fullfile(PATH_TO_IMG_FOLDER, '0002.jpeg'));
+[H, r, c] = HarrisCornerDetector(I1, SIGMA, NEIGHBOURS, THRESHOLD, 0);
+
+optical_flow = lucas_kanade(I1, I2, 15);
+
+% Using the optical flow, use coordinates r,c = (y,x) to find the points
+% where the optical_flow starts from Lucas_Kanade.
+
+% Then, the points on image-2 will be the next starting points.
+
+% On image 3, we will again calcualte the optical-flow, and map the points
+% from image-2 to image 3.
