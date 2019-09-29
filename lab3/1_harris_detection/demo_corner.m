f6b91a37a1e07533fee80c6d3f1bc56a4541b2de
amandavinci <amanthevinci@gmail.com>
2019-09-29 13:53:11 +0200

    enhance image plotting

--- lab3/1_harris_detection/demo_corner.m
+++ lab3/1_harris_detection/demo_corner.m
@@ -6,7 +6,7 @@ threshold = 0.05;
 [H, r, c] = HarrisCornerDetector(Img1, sigma, neighbors, threshold, 0);
 figure;
 imshow(Img1); hold on;
-scatter(c, r, 20, 'r');
+scatter(c, r, 30, 'y');
 title("Sample: Person Toy"); hold off;
 
 Img2 = imread('../pingpong/0000.jpeg');
@@ -14,7 +14,7 @@ threshold = 0.2;
 [H, r, c] = HarrisCornerDetector(Img2, sigma, neighbors, threshold, 0);
 figure;
 imshow(Img2); hold on;
-scatter(c, r, 20, 'r');
+scatter(c, r, 30, 'y');
 title("Sample: Pingpong"); hold off;   
 
 Img3 = imrotate(Img1, 90);
@@ -22,7 +22,7 @@ threshold = 0.05;
 [H, r, c] = HarrisCornerDetector(Img3, sigma, neighbors, threshold, 0);
 figure;
 imshow(Img3); hold on;
-scatter(c, r, 20, 'r');
+scatter(c, r, 30, 'y');
 title("Sample: Person Toy [Rotated 90]"); hold off;
 
 Img4 = imrotate(Img1, 45);
@@ -30,5 +30,5 @@ threshold = 0.05;
 [H, r, c] = HarrisCornerDetector(Img4, sigma, neighbors, threshold, 0);
 figure;
 imshow(Img4); hold on;
-scatter(c, r, 20, 'r');
+scatter(c, r, 30, 'y');
 title("Sample: Person Toy [Rotated 45]"); hold off;
