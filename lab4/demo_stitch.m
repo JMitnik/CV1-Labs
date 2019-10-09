05e440f7f32dabace60f6f59dede31a849246a29
amandavinci <amanthevinci@gmail.com>
2019-10-09 13:48:28 +0200

    stitching function and demo

--- lab4/demo_stitch.m
+++ lab4/demo_stitch.m
@@ -1,46 +1,5 @@
-%Reading in and transforming the pictures to the same sizes
-
-%Comment this if to see the boat pictures
-left=rgb2gray(imread("left.jpg"));
-right=rgb2gray(imread("right.jpg"));
-right=(imresize(right,size(left)));
-
-im1=left;
-im2=right;
-
-%Uncomment this to see the relatively well working pictures of the boats
-%im1=imread("boat1.pgm");
-%im2=imread("boat2.pgm");
-
-%Plotting the matches
-keypoint_matchings=keypoint_matching(im1,im2);
-mathces=sample_keypoint_matchings(keypoint_matchings,10);
-plot_matches(im1,im2,mathces);
-
-%This is Jonathan's code
-default_N = 10;
-default_P = 10;
-radius_size = 10;
-best_transformation = RANSAC(keypoint_matchings, im1, im2, default_N, default_P, radius_size, false);
-
-% Perform transformation from im1 to im2, and back (im2 to im1)
-T = [best_transformation(1), best_transformation(3),0
-    best_transformation(2), best_transformation(4), 0
-    best_transformation(5), best_transformation(6), 1];
-transformation = affine2d(T);
-
-figure(2);
-im1_to_im2 = imwarp(im1, transformation);
-imshow(im1_to_im2);
-title('Image 1 transformed to image 2');
-
-figure(3);
-im2_to_im1 = imwarp(im2, invert(transformation));
-imshow(im2_to_im1);
-title('Image 2 transformed to image 1');
-
-%As you can see, the transformations are sometimes not invertable and MATLAB gives
-%an error, sometimes they are just really bad. Im not sure why it is. Maybe
-%because the mathcing feature point pairs are on different sides. In image
-%"right.jpg" htey are on the left side, in image "left.jpg" they are on the
-%right side.
+im1 = 'boat1.pgm';
+im2 = 'boat2.pgm';
+stitched_image = stitch(im1, im2);
+figure;
+imshow(stitched_image);
--- lab4/stitch.m
+++ lab4/stitch.m
@@ -1,8 +1,63 @@
-function [stichted_image] = stitch(im1, im2)
+function [stitched_image] = stitch(im1, im2)
 %stitch - Description
 %
-% Syntax: [stichted_image] = stitch(im1, im2)
+% Syntax: [stitched_image] = stitch(im1, im2)
 %
 % Long description
+
+    im1 = imread(im1);
+    im2 = imread(im2);
+    if size(im1,3) == 3 && size(im2,3) == 3 
+        im1 = rgb2gray(imread(im1));
+        im2 = rgb2gray(imread(im2));
+    end
+
+    [fa, da] = vl_sift(single(im1), 'PeakThresh', 0);
+    [fb, db] = vl_sift(single(im2), 'PeakThresh', 0);
+    [matches, scores] = vl_ubcmatch(da, db);
+
+    x1 = fa(1,matches(1,:));
+    y1 = fa(2,matches(1,:));
+    x2 = fb(1,matches(2,:));
+    y2 = fb(2,matches(2,:));
+    keypoint_matchings = [x1; y1; x2; y2];
+    default_N = 10;
+    default_P = 10;
+    radius_size = 10;
+    best = RANSAC(keypoint_matchings, im1, im2, default_N, default_P, radius_size, false);
+    
+    % Transform
+    Ia = im2double(im1);
+    Ib = im2double(im2);
+    m = [best(1), best(2); best(3), best(4)];
+    t = [best(5); best(6)];
+    for x = 1:1:size(Ia, 2)
+        for y = 1:1:size(Ia, 1)
+            points = round(m*[x; y] + t);
+            if points(1) > 0 && points(2) > 0
+                Ia_trans(points(2), points(1), :) = Ia(y, x, :);
+            end
+        end
+    end
+
+    % enlarging the canvas to accomodate out of frame transformations
+    delta_width = size(Ia_trans,1) - size(Ib,1);
+    delta_height = size(Ia_trans,2) - size(Ib,2); 
+    pad_a = [max(-delta_width,0) max(-delta_height,0)];
+    pad_b = [max(delta_width,0) max(delta_height,0)];
+    Ia_trans = padarray(Ia_trans, pad_a, 'post');
+    Ib = padarray(Ib, pad_b, 'post');
+
+    % Stitch
+    for x = 1:1:size(Ib, 1)
+        for y = 1:1:size(Ib, 2)
+            if Ia_trans(x, y) == 0
+                I_ab(x, y, :) = Ib(x, y, :);
+            else
+                I_ab(x, y, :) = Ia_trans(x, y, :);
+            end
+        end
+    end
+    stitched_image = I_ab;
 
 end
