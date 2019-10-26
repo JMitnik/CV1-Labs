1b22129a605ee31ebfe47b19e3e5fe2e824cebba
Jonathan Mitnik <jmitnik@gmail.com>
2019-10-26 12:39:13 +0200

    Better results

--- lab-final/part1-svm/jonathan-impl/main.m
+++ lab-final/part1-svm/jonathan-impl/main.m
@@ -1,6 +1,7 @@
 % ! NOTE: Run VL_Setup.m to get started
 % Default params
-SAMPLE_SIZE = 20;
+SAMPLE_SIZE_TRAIN = 100;
+SAMPLE_SIZE_TEST = 100;
 VOCAB_SIZE = 400;
 COLOR_SPACE = 'greyscale_SIFT';
 SAMPLING_MODE = 'keypoints_SIFT';
@@ -9,23 +10,15 @@ SAMPLING_MODE = 'keypoints_SIFT';
 % 0. Training loading
 %%%
 path_to_train = 'data/train.mat';
-train_full = load(path_to_train);
-
-train_X = train_full.X;
-train_y = train_full.y;
-labels = train_full.class_names;
-
-% Debug-mode: sample number of points per class
-[sampled_train_X, sampled_train_Y] = sample_points_per_class(train_X, train_y, SAMPLE_SIZE);
-train_X = sampled_train_X;
-train_y = sampled_train_Y;
-
-% Points used for the vocab: half of the original
-vocab_X = sample_points_per_class(train_X, train_y, floor(SAMPLE_SIZE / 2));
+path_to_test = 'data/test.mat';
+[train_X, train_y, test_X, test_y] = read_input(path_to_train, path_to_test, true, SAMPLE_SIZE_TRAIN, SAMPLE_SIZE_TEST);
 
 %%%
 % 1. Feature extraction
 %%%
+% Points used for the vocab: half of the original
+vocab_X = sample_points_per_class(train_X, train_y, floor(SAMPLE_SIZE / 2));
+
 feature_descriptors = extract_features(vocab_X, COLOR_SPACE, SAMPLING_MODE);
 
 %%%
@@ -37,12 +30,39 @@ vocab = create_vocab(feature_descriptors, VOCAB_SIZE);
 % 3. Quantize features using a visual vocabulary
 % 4. Represent features by frequencies
 %%%
-encodings = encode_images(train_X, vocab, COLOR_SPACE, SAMPLING_MODE);
+train_encodings = encode_images(train_X, vocab, COLOR_SPACE, SAMPLING_MODE);
+test_encodings = encode_images(test_X, vocab, COLOR_SPACE, SAMPLING_MODE);
 
 %%%
 % 5. Classification
 %%%
-model = svmtrain(train_y, encodings);
+%# train one-against-all models
+model = cell(length(label_idxs),1);
+
+for i=1:length(label_idxs)
+    class_label = label_idxs(i);
+    binary_classes = train_y == class_label;
+    model{i} = svmtrain(double(binary_classes), train_encodings, '-c 1 -g 0.2 -b 1');
+end
+
+%# get probability estimates of test instances using each model
+prob = zeros(size(test_X, 1), length(label_idxs));
+
+for i=1:length(label_idxs)
+    class_label = label_idxs(i);
+    binary_classes = test_y == class_label;
+    [~,~,p] = svmpredict(double(binary_classes), test_encodings, model{i}, '-b 1');
+    prob(:,i) = p(:,model{i}.Label==1);    %# probability of class==k
+end
+
+% MANUEL DEBUG
+[~,pred] = max(prob,[],2);
+pred(pred==4) = 7;
+pred(pred==5) = 9;
+
+%# predict the class with the highest probability
+acc = sum(pred == test_y) ./ numel(test_y);    %# accuracy
+C = confusionmat(test_y, pred);                   %# confusion matrix
 
 %%%
 % 6. Evaluation
---
+++ lab-final/part1-svm/jonathan-impl/read_input.m
@@ -0,0 +1,29 @@
+function [train_X, train_y, test_X, test_y] = read_input(path_to_train, path_to_test, DEBUG_MODE, SAMPLE_SIZE_TRAIN, SAMPLE_SIZE_TEST)
+    path_to_train = 'data/train.mat';
+    path_to_test = 'data/test.mat';
+
+    train_full = load(path_to_train);
+    test_full = load(path_to_train);
+
+    label_idxs = [1,2,3,7,9];
+    train_X = train_full.X;
+    train_y = train_full.y;
+    labels = train_full.class_names(label_idxs);
+
+    test_X = test_full.X;
+    test_y = test_full.y;
+
+    % TODO:  Last step - Filter only on the 5 labels
+
+    if DEBUG_MODE == true
+    % Debug-mode: sample number of points per class
+        [sampled_train_X, sampled_train_y] = sample_points_per_class(train_X, train_y, SAMPLE_SIZE_TRAIN);
+        [sampled_test_X, sampled_test_y] = sample_points_per_class(test_X, test_y, SAMPLE_SIZE_TEST);
+
+        train_X = sampled_train_X;
+        train_y = sampled_train_y;
+
+        test_X = sampled_test_X;
+        test_y = sampled_test_y;
+    end
+end
